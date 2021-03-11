9cf8b302305ac9d78c287f036a82115a676cab23
skarolk <skarolkiewicz@gmail.com>
2021-03-10 22:04:03 -0600

    set up schema

--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,11 @@
 const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
+const _ = require("lodash");
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+
+const users = [
+  { id: "23", firstName: "Bob", lastName: "Ross", age: 80 },
+  { id: "47", firstName: "Steve", lastName: "Ross", age: 54 },
+];
 
 const UserType = new GraphQLObjectType({
   name: "User",
@@ -10,3 +16,20 @@ const UserType = new GraphQLObjectType({
     age: { type: GraphQLInt },
   },
 });
+
+// root query tells graphql where to jump into initial piece of data
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      // resolve reaches out and grabs real data
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id });
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({ query: RootQuery });
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+
 const app = express();
 
 app.use(
   "/graphql",
   graphqlHTTP({
     // graphiql for development only
+    // schema: schema
+    schema,
     graphiql: true,
   })
 );
